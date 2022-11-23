89e4f17e9d72be548df9b4d6e2fb1a06e7d79756
KleerZ <escapeobzory@gmail.com>
2022-11-23 17:54:58 +0300

    add user model

--- Task.Mvc/Controllers/HomeController.cs
+++ Task.Mvc/Controllers/HomeController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Task.Mvc.Models;
 
 namespace Task.Mvc.Controllers;
 
 public class HomeController : Controller
 {
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(UserViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View(model);
+
+        return RedirectToAction("Index", "Message");
     }
 }
