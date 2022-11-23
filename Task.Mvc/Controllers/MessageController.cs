a2ea1967f3a622deeb5be935fb6bee138d340f5b
KleerZ <escapeobzory@gmail.com>
2022-11-23 17:43:18 +0300

    add message page

---
+++ Task.Mvc/Controllers/MessageController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task.Mvc.Controllers;
+
+public class MessageController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- Task.Mvc/wwwroot/js/site.js
+++ Task.Mvc/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let messages = document.getElementsByClassName('message')
+for (let i = 0; i < messages.length; i++) {
+    messages[i].addEventListener("click", function (e){
+        e.preventDefault();
+        showMessage(messages[i])
+    })
+}
 
-// Write your JavaScript code.
+function showMessage(element){
+    let title = element.getElementsByClassName('message-title')[0].innerHTML;
+    let sender = element.getElementsByClassName('message-sender')[0].innerHTML;
+    let date = element.getElementsByClassName('message-date')[0].innerHTML;
+    let body = element.getElementsByClassName('message-body')[0].innerHTML;
+    
+    let modalTitle = document.getElementById('modal-title')
+    let modalSender = document.getElementById('modal-sender')
+    let modalDate = document.getElementById('modal-date')
+    let modalBody = document.getElementById('modal-body')
+    
+    modalTitle.innerHTML = title
+    modalSender.innerHTML = sender
+    modalDate.innerHTML = date
+    modalBody.innerHTML = body
+}
