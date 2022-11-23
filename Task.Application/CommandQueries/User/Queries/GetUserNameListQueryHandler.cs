428814d6dfa631dc21321a1893df2a68a34b46dd
KleerZ <escapeobzory@gmail.com>
2022-11-23 20:09:51 +0300

    add get usernames query

---
+++ Task.Application/CommandQueries/User/Queries/GetUserNameListQueryHandler.cs
@@ -0,0 +1,24 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Task.Application.Common.Interfaces;
+
+namespace Task.Application.CommandQueries.User.Queries;
+
+public class GetUserNameListQueryHandler : IRequestHandler<GetUserNameListQuery, UserNameListVm>
+{
+    private readonly IApplicationContext _context;
+
+    public GetUserNameListQueryHandler(IApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<UserNameListVm> Handle(GetUserNameListQuery request,
+        CancellationToken cancellationToken)
+    {
+        var usernames = await _context.Users
+            .Select(u => u.Name).ToListAsync(cancellationToken);
+
+        return new UserNameListVm { UserNames = usernames };
+    }
+}
